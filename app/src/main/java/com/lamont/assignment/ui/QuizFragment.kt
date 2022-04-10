ee2af2632d13a1a471cbbbb6b936556e5d3277ce
Nick <nicholasyqn-wm20@student.tarc.edu.my>
2022-04-11 00:32:35 +0800

    nicholas - 11/4

--- app/src/main/java/com/lamont/assignment/ui/QuizFragment.kt
+++ app/src/main/java/com/lamont/assignment/ui/QuizFragment.kt
@@ -1,23 +1,84 @@
 package com.lamont.assignment.ui
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.lamont.assignment.R
+import android.widget.Toast
+import com.google.firebase.firestore.*
+import com.lamont.assignment.adapter.QuizAdapter
+import com.lamont.assignment.model.QuizMenu
+import com.lamont.assignment.databinding.FragmentQuizBinding
 
 class QuizFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentQuizBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var quizAdapter: QuizAdapter
+    private lateinit var dbQuiz: FirebaseFirestore
+    private var selectedQuiz : String? = null
+    private var selectedQuizDuration: Long? = 0
+    private var quizQues: MutableList<QuizMenu> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz, container, false)
+
+        _binding = FragmentQuizBinding.inflate(inflater, container, false)
+        //Inflate the layout for this fragment
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Instantiate Firestore database
+        dbQuiz = FirebaseFirestore.getInstance()
+
+        //Collecting individual quiz data from Firestore and instantiate an object (Quiz menu)
+        dbQuiz.collection("quiz")
+            .get()
+            .addOnSuccessListener {
+                for (document in it) {
+                    val title: String = document.get("title").toString()
+                    val duration: Long = document.get("duration") as Long
+                    val image: String = document.get("image").toString()
+
+                    quizQues.add(QuizMenu(title, duration, image))
+                }
+
+                //Inflate layout
+                quizAdapter = QuizAdapter(requireContext(),quizQues)
+                binding.rvQuiz.adapter = quizAdapter
+                //User will be directed to selected quiz where relevant questions will be displayed
+                quizAdapter.setOnItemClickListener(object : QuizAdapter.OnItemClickListener{
+                    override fun onItemClicked(position: Int) {
+                        selectedQuiz = quizQues[position].title
+                        selectedQuizDuration = quizQues[position].duration
+                        Toast.makeText(requireContext(),selectedQuiz, Toast.LENGTH_SHORT).show()
+                        Intent(requireContext(), QuizActivity::class.java).apply {
+                            //Passing data to Quiz Activity
+                            putExtra("selectedQuiz", selectedQuiz)
+                            putExtra("duration", selectedQuizDuration)
+                            startActivity(this)
+                        }
+                    }
+                })
+            }
+            .addOnFailureListener { error ->
+                Log.w(TAG, "Firebase error.", error)
+            }
+
+        //Refreshing page
+        binding.swipeToRefresh.setOnRefreshListener {
+            binding.swipeToRefresh.isRefreshing = false
+            binding.rvQuiz.adapter = quizAdapter
+        }
+    }
+
 }
